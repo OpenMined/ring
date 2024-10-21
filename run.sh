20e20f6472ae7ba1f889269e52b9ee44bb23f382
Shubham Gupta <shubhamgupta3121@gmail.com>
2024-10-21 14:23:04 +0530

    install pip for the python version installed by pip

--- run.sh
+++ run.sh
@@ -3,8 +3,11 @@
 # this will create venv from python version defined in .python-version
 uv venv
 
+
+uv run python -m ensurepip --upgrade
+
 # install requirements for the project
 uv pip install -r requirements.txt
 
 # run app using python from venv
-uv run main.py
++uv run main.py 2>&1 | tee app.log
