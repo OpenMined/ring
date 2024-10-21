82aba9d34372d5299bceb97dbf4c79d6750d0bba
Shubham Gupta <shubhamgupta3121@gmail.com>
2024-10-21 17:49:20 +0530

    install from requirements.txt using app's venv python pip

--- run.sh
+++ run.sh
@@ -3,11 +3,16 @@
 # this will create venv from python version defined in .python-version
 uv venv
 
-
+# Ensure pip is installed and up-to-date
 uv run python -m ensurepip --upgrade
 
+# Note: we need to point to the pip against the python version
+# since uv is sensitive to current environment
+# so running this app from syftbox uses syftbox's uv environment, 
+# instead of the app environent
+
 # install requirements for the project
-uv pip install -r requirements.txt
+uv run python -m pip install -r requirements.txt
 
 # run app using python from venv
-+uv run main.py 2>&1 | tee app.log
+uv run main.py 2>&1 | tee app.log
