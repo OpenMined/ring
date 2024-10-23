9bc655c268097fac7c358ac64cfbda898cd027a5
IonesioJunior <ionesiojr@gmail.com>
2024-10-23 19:45:44 -0300

    Change ring field name to participants

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ RING_APP_PATH = Path(os.path.abspath(__file__)).parent
 
 
 class RingData(BaseModel):
-    ring: list[str]
+    participants: list[str]
     data: int
     current_index: int
 
@@ -23,14 +23,17 @@ class RingData(BaseModel):
         with open(file, "r") as f:
             return cls(**from_json(f.read()))
 
+
 class RingRunner:
     def __init__(self):
         self.client = Client.load()
 
         self.my_email: str = self.client.email
-        
+
         # this is where all the app state goes
-        self.ring_pipeline_path: Path = Path(self.client.datasite_path) / "app_pipelines" / "ring"
+        self.ring_pipeline_path: Path = (
+            Path(self.client.datasite_path) / "app_pipelines" / "ring"
+        )
         # this is where the pending inputs go
         self.running_folder: Path = self.ring_pipeline_path / "running"
         # this is where the final result goes of a completed ring
@@ -46,7 +49,7 @@ class RingRunner:
 
         if len(input_files) == 0:
             print("No data file found. As you were, soldier.")
-        
+
     def process_input(self, file_path) -> None:
         print(f"Found input {file_path}! Let's get to work.")
 
@@ -56,7 +59,7 @@ class RingRunner:
         next_index = ring_data.current_index + 1
 
         if next_index < ring_data.ring_length:
-            next_person = ring_data.ring[next_index]
+            next_person = ring_data.participants[next_index]
             ring_data.current_index = next_index
             self.send_data(next_person, ring_data)
         else:
@@ -75,14 +78,16 @@ class RingRunner:
 
         # after this there will be files (so we can sync)
         permission = SyftPermission.mine_with_public_write(self.my_email)
-        permission.ensure(self.ring_pipeline_path)  
+        permission.ensure(self.ring_pipeline_path)
 
     def my_secret(self):
         with open(self.secret_file, "r") as secret_file:
             return int(secret_file.read().strip())
 
     def pending_inputs_files(self) -> List[Path]:
-        return [self.running_folder / file for file in self.running_folder.glob("*.json")]
+        return [
+            self.running_folder / file for file in self.running_folder.glob("*.json")
+        ]
 
     def write_json(self, file_path: Path, result: RingData) -> None:
         print(f"Writing to {file_path}.")
@@ -92,7 +97,13 @@ class RingRunner:
 
     def send_data(self, email: str, data: RingData) -> None:
         destination_datasite_path = Path(self.client.sync_folder) / email
-        dest = destination_datasite_path / "app_pipelines" / "ring" / "running" / "data.json"
+        dest = (
+            destination_datasite_path
+            / "app_pipelines"
+            / "ring"
+            / "running"
+            / "data.json"
+        )
         self.write_json(dest, data)
 
     def terminate_ring(self, data: RingData) -> None:
@@ -103,4 +114,3 @@ class RingRunner:
 if __name__ == "__main__":
     runner = RingRunner()
     runner.run()
-    
