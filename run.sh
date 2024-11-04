1a462233593cdec00ea6abdf7ac270c55cbf70fa
dk <dkn.work@protonmail.com>
2024-11-04 10:25:50 +0700

    fix ring app not run with error `no module named syftbox` if running in a python env with python different than 3.12

--- run.sh
+++ run.sh
@@ -5,6 +5,9 @@ set -e
 # this will create venv from python version defined in .python-version
 uv venv
 
+# Activate the virtual environment using the dot command
+. .venv/bin/activate
+
 # install requirements for the project
 uv pip install -r requirements.txt
 
