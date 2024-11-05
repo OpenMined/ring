923a2feed55d98a037e5958caccbbdbfd28ffbda
dk <dkn.work@protonmail.com>
2024-11-05 09:27:11 +0700

    - use `python3` instead of `uv run` after directly activating the env - deactivate after running `main.py`
    Co-authored-by: Yash Gorana <yash.gorana@hotmail.com>

--- run.sh
+++ run.sh
@@ -12,4 +12,8 @@ uv venv
 uv pip install -r requirements.txt
 
 # run app using python from venv
-uv run main.py
+echo "Running Ring with $(python3 --version) at '$(which python3)'"
+python3 main.py
+
+# deactivate the virtual environment
+deactivate
