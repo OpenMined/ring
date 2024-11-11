6345bb10eef49226686fc08069e89df0025eee57
Yash Gorana <yash.gorana@hotmail.com>
2024-11-11 15:45:24 +0530

    upgrade syftbox

--- run.sh
+++ run.sh
@@ -3,13 +3,15 @@
 set -e
 
 # this will create venv from python version defined in .python-version
-uv venv
+if [ ! -d .venv ]; then
+  uv venv
+fi
 
 # Activate the virtual environment using the dot command
 . .venv/bin/activate
 
 # install requirements for the project
-uv pip install -r requirements.txt
+uv pip install --upgrade -r requirements.txt
 
 # run app using python from venv
 echo "Running Ring with $(python3 --version) at '$(which python3)'"
