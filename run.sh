6c2aff333b99ad5988de98d1d92333b33dc350d2
Ionésio Junior <ionesiojr@gmail.com>
2024-11-13 19:12:30 -0300

    Update run.sh
    Force python 3.12 venv

--- run.sh
+++ run.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
-
 set -e
 
-# this will create venv from python version defined in .python-version
-if [ ! -d .venv ]; then
-  uv venv
+if [ ! -d ".venv" ]; then
+    echo "Virtual environment not found. Creating one..."
+    uv venv -p 3.12 .venv
+    echo "Virtual environment created successfully."
+    uv pip install -U syftbox
+    # uv pip install -r requirements.txt
+else
+    echo "Virtual environment already exists."
 fi
 
-# Activate the virtual environment using the dot command
 . .venv/bin/activate
 
-# install requirements for the project
-uv pip install --upgrade -r requirements.txt --quiet
+# # run app using python from venv
+echo "Running ring with $(python3 --version) at '$(which python3)'"
+uv run python3 main.py
 
-# run app using python from venv
-echo "Running Ring with $(python3 --version) at '$(which python3)'"
-python3 main.py
-
-# deactivate the virtual environment
+# # deactivate the virtual environment
 deactivate
